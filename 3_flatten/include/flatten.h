95c9b7b7e1153d4a991e82152888aa09d9b4ddaa
hangedplane <hangedplane@gmail.com>
2017-10-26 11:52:36 +0300

    add Flatten hw

--- 3_flatten/include/flatten.h
+++ 3_flatten/include/flatten.h
@@ -1,10 +1,10 @@
 //
-// Created by eugene on 11.10.2017
+// Created by Sergey on 11.10.2017
 //
 
 #pragma once
 
-
+#include <type_traits>
 #include "../../2_promise_future/include/future.h"
 
 template<typename T>
@@ -12,13 +12,52 @@ Future<T> Flatten(Future<T> const & that) {
     return that;
 }
 
-template<typename T>
-Future<T> Flatten(Future<Future<T>> const & that) {
-    return Flatten(that.Get());
+template <typename T>
+struct Impl;
+
+template <typename T>
+struct Impl<Future<T>> {
+    using inner_t = T;
+};
+
+template <typename T>
+struct Impl<const Future<T> &> {
+    using inner_t = typename Impl<T>::inner_t;
+};
+
+template <typename T>
+struct Impl<Future<Future<T>>> {
+    using inner_t = typename Impl<Future<T>>::inner_t;
+};
+
+template <typename T>
+Future<typename Impl<T>::inner_t> Flatten(Future<Future<T>> const & that) {
+    Promise<typename Impl<T>::inner_t> promise;
+    return promise.GetFuture();
 }
 
 template<template<typename, typename...> class C, typename T>
 Future<C<T>> Flatten(C<Future<T>> const & that) {
-    Future<C<T>> result;
-    return result;
+    C<T> data(that.size());
+    auto curr = data.begin();
+    for (auto item = that.begin(); item != that.end(); ++item) {
+        *curr = item->Get();
+        ++curr;
+    }
+    Promise<C<T>> promise;
+    promise.Set(data);
+    return promise.GetFuture();
 }
+
+//template<template<typename, typename...> class C, typename T, typename... Targs>
+//typename std::enable_if<&C<Future<T>, Targs...>::begin != nullptr, Future<C<T, Targs...>>>::type Flatten(C<Future<T>, Targs...> const & that) {
+//    C<T> data(that.size());
+//    auto curr = data.begin();
+//    for (auto item = that.begin(); item != that.end(); ++item) {
+//        *curr = item->Get();
+//        ++curr;
+//    }
+//    Promise<C<T>> promise;
+//    promise.Set(data);
+//    return promise.GetFuture();
+//}
